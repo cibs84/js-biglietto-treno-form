7c58572fd42f60e636270b67f90ea0411091a8af
cibs84 <d.tilesi@gmail.com>
2022-06-06 18:41:18 +0200

    Improved Js comments

--- js/script.js
+++ js/script.js
@@ -5,11 +5,12 @@
 // va applicato uno sconto del 40% per gli over 65.
 // Il recap dei dati e l'output del prezzo finale va stampato in pagina (formattato con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
-// RACCOLTA DATI - Chiedo all'utente di inserire NOME E COGNOME, KM DA PERCORRERE, FASCIA D'ETA'
+// GENERATE BUTTON'S FUNCTION
 let genBtn = document.getElementById('generate-btn');
 
 genBtn.addEventListener('click',
     function () {
+        // RACCOLTA DATI di: NOME E COGNOME, KM DA PERCORRERE, FASCIA D'ETA'
         const userName = document.getElementById('user-name').value;
         const userKm = parseInt( document.getElementById('user-km').value );
         const userAge = document.getElementById('user-age').value;
@@ -29,29 +30,33 @@ genBtn.addEventListener('click',
         // OUTPUT - STAMPA SU HTML: NOME DEL PASSEGGERO, OFFERTA, CARROZZA, CODICE CP, COSTO BIGLIETTO
         document.getElementById('ticket-name').innerHTML = userName;
         document.getElementById('ticket-type').innerHTML = ticketType;
-
-        const carriage = Math.floor(Math.random() * 20) + 1;
+            // Numero carrozza random tra 1 e 20
+            const carriage = Math.floor(Math.random() * 20) + 1;
         document.getElementById('carriage').innerHTML = carriage;
-
-        const cpCode = Math.floor(Math.random() * 99000) + 1;
-        document.getElementById('cp-code').innerHTML = cpCode;
-        
+            // Numero Codice CP random tra 1 e 99999
+            const cpCode = Math.floor(Math.random() * 99999) + 1;
+        document.getElementById('cp-code').innerHTML = cpCode;        
         document.getElementById('ticket-price').innerHTML = `${price.toFixed(2)}€`;
+
+        // Attribuzione classe 'active' agli elementi html per visualizzare i dati del biglietto
         document.getElementById('your-ticket').classList.toggle('active');
         document.querySelector('h2').classList.toggle('active');
     }
 );
 
+// CANCEL BUTTON'S FUNCTION
 let cancBtn = document.getElementById('cancel-btn');
 
 cancBtn.addEventListener('click',
     function(){
+        // RESET INPUTS VALUES
         document.getElementById('ticket-name').value = '';
         document.getElementById('ticket-type').value = 'maggiorenne';
         document.getElementById('carriage').value = '';
         document.getElementById('cp-code').value = '';
         document.getElementById('ticket-price').value = '';
 
+        // OUTPUT TICKET REMOVAL
         document.getElementById('your-ticket').classList.remove('active');
         document.querySelector('h2').classList.remove('active');
     }
