d3c4efda34cd51ea22c51d59aa850c057120a3d6
cibs84 <d.tilesi@gmail.com>
2022-06-06 15:22:54 +0200

    Structures HTML, CSS and JS

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Il programma dovrà mostrare una form da compilare con cui chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65.
+// Il recap dei dati e l'output del prezzo finale va stampato in pagina (formattato con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+// RACCOLTA DATI - Chiedo all'utente di inserire NOME E COGNOME, KM DA PERCORRERE, FASCIA D'ETA'
+
+
+// LOGICA - CREAZIONE VARIABILI PREZZI STANDARD, MINORENNI, OVER_65
+
+
+// STAMPA SU HTML IL NOME DEL PASSEGGERO, OFFERTA, CARROZZA, CODICE CP, COSTO BIGLIETTO
